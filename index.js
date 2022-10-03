3d4b8d28ce360dd2c9e1ce1d52d1f7fa5f8523b6
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-03 06:18:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const name =() =>{
+  // console.log("Hello")
+  return "kkrh"
+}
+// function averg(x,y){
+//   // console.log("done")
+//   return (x+y)/2
+// }
+const sum = ( p ,q)=>{
+  return p+q
+}
+
+let a = 5;
+let b = 10;
+let c = 15 ;
+
+// console.log("average of a and b is ",averg(a,b))
+// console.log("average of b and c is ",averg(b,c))
+// console.log("average of c and a is ",averg(c,a))
+
+// let s = name();
+console.log(name())
+console.log("sum of a and b is ",sum(a,b))
+console.log("sum of b and c is ",sum(b,c))
+console.log("sum of c and a is ",sum(c,a))
